a2c4ea2b3fc7bc0bd2b12afa7d8800f57dcb5907
DaveRainbowin <51142390+DaveRainbowin@users.noreply.github.com>
2020-08-22 21:46:07 -0500

    you've been saved

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+
+## [0.1.1] - 2020-08-22
+### Added
+- Saving the game (and autosaving/loading)
+
+## [0.1.0] - 2020-08-22
 ### Added
 - RAM from [Reinhardt-C](https://github.com/Reinhardt-C)
 - whattheburger from [DaveRainbowin](https://github.com/DaveRainbowin)
--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var ramLeft = 1000;
+var ramTotal = 1000;
 var ramTab = 50;
 var tabs = 0;
 var collapseD = false;
+var cardinals = 0;
+var cardinalGain = 0;
 setInterval(update, 100);
 function openTab() {
   ramLeft -= ramTab;
@@ -9,6 +12,8 @@ function openTab() {
 }
 function update() {
   get("ramLeft").innerHTML = (ramLeft >= 1000) ? `${ramLeft / 1000}kB` : `${ramLeft}B`;
+  get("tabsOpen").innerHTML = `${tabs} tabs`;
+  cardinalGain = Math.log10(ramTotal).toFixed(0);
   if (ramLeft <= 0 && !collapseD) {
     collapse();
   }
@@ -20,10 +25,52 @@ function collapse() {
   let collapseText = document.createTextNode("YOU HAVE COLLAPSED!");
   collapsed.appendChild(collapseText);
   collapsed.setAttribute("style", "color: lime; font-size: 175px; font-family: Comic Sans MS");
+  collapsed.setAttribute("id", "collapseText");
   document.body.insertBefore(collapsed, get("ramLeft"));
   collapseD = true;
-  // setTimeout(uncollapse(), 3000);
+  setTimeout(uncollapse(), 3000);
 }
+function uncollapse() {
+  cardinals += cardinalGain;
+  get("collapseText").style.display = "none";
+  document.body.style.backgroundColor = "white";
+  ramLeft = ramTotal;
+  collapseD = false;
+}
+
 function get(id) {
   return document.getElementById(id);
 }
+var listofstuff = ["ramLeft", "ramTotal", "ramTab", "tabs", "collapseD", "cardinals", "cardinalGain"];
+var saveload = {
+  save: function() {
+    listofstuff.forEach(x => localStorage.setItem(x, JSON.stringify(window[x])));
+  },
+  load: function() {
+    visitedBefore = JSON.parse(localStorage.getItem('visitedBefore'));
+    if (visitedBefore == null || visitedBefore == undefined) {
+      visitedBefore = false;
+    }
+    if (visitedBefore) {
+      listofstuff.forEach(x => window[x] = JSON.parse(localStorage.getItem(x)));
+    }
+  },
+  init: function() {
+    setTimeout(saveload.activateAutosave, 5000);
+    saveload.load();
+    if (!visitedBefore) {
+      visitedBefore = true;
+      saveload.save();
+    }
+  },
+  activateAutosave: function() {
+    setInterval(saveload.save, 5000);
+  },
+  wipeSave: function() {
+    let listofdefault = [1000, 1000, 50, 0, false, 0, 0];
+    for (i = 0; i <= listofdefault.length; i++) {
+      window[listofstuff[i]] = listofdefault[i];
+    }
+  }
+};
+setTimeout(saveload.init, 1000);
