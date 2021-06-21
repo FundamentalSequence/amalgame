640c04684c9831a8919a989d12592eeab3ec8a17
FundamentalSequence <iamnotachuckster@gmail.com>
2021-06-21 09:06:01 -0500

    fixed pretty()

--- script.js
+++ script.js
@@ -56,8 +56,8 @@ function openTab() {
 }
 function update() {
   function pretty(num) {
-    if (num >= 1000000000) {return `${(num/1000).toPrecision(4)}GB`}
-    if (num >= 1000000) {return `${(num/1000).toPrecision(4)}MB`}
+    if (num >= 1000000000) {return `${(num/1000000000).toPrecision(4)}GB`}
+    if (num >= 1000000) {return `${(num/1000000).toPrecision(4)}MB`}
     if (num >= 1000) {return `${(num/1000).toPrecision(3)}KB`}
     return `${num}B`
   }
